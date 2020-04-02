81914d86f13946578a357be9c54873a6bb18e203
XiakaiPan <13212017962@163.com>
2020-04-02 21:18:19 +0800

    recursion_dynamic_programming

--- CS6Recursion/TrinomialDP.java
+++ CS6Recursion/TrinomialDP.java
@@ -5,11 +5,23 @@
  **************************************************************************** */
 
 public class TrinomialDP {
-
     // Returns the trinomial coefficient T(n, k).
     public static long trinomial(int n, int k) {
+        long[][] f = new long[n + 1][n + 2];
+        f[0][0] = 1;
+        // System.out.println(f[n][k]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (j < -n || j > n) {
+                    f[i][j] = 0;
+                }
+                else {
+                    f[i][j] = f[i - 1][j - 1] + f[i - 1][j] + f[i - 1][j + 1];
+                }
+            }
+        }
+        return f[n][k];
     }
-
 
     // Takes two integer command-line arguments n and k and prints T(n, k).
     public static void main(String[] args) {
